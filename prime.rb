81bf0c8af119c83310710591fc9671336566ab74
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 00:40:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(arg)
+  range = (2..arg)
+  newArray= range.to_a 
+  counter = 0 
+  bool = true 
+  while (counter < newArray.size)
+    counter = 2 
+    
+    counter = counter  + 1 
+end 
